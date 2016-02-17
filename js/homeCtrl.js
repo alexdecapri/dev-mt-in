f48b7df99364a249ee9fc359a18f877a94ff4077
alexdecapri <alexdecapri@gmail.com>
2016-02-16 20:28:41 -0700

    finished Day One

--- js/homeCtrl.js
+++ js/homeCtrl.js
@@ -5,4 +5,14 @@ app.controller("homeCtrl", function($scope) {
 		// name: "Alex DeCapri",
 		friends: [{name: 'Ryan'}, {name: 'Bryan'}, {name: 'Sarah'}, {name: 'Zac'}, {name: 'Erin'}]
 	};
+
+	$scope.sortOptions = [{
+		display: "Ascending",
+		value: false
+		},
+		{
+		display: "Descending",
+		value: true
+		}
+	];
 });
