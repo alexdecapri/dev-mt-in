9fef1904b3a43b9c52eeef482d4e6e5e8ff284b6
alexdecapri <alexdecapri@gmail.com>
2016-02-18 20:14:51 -0700

    finished day 2

--- js/homeCtrl.js
+++ js/homeCtrl.js
@@ -1,10 +1,10 @@
 var app = angular.module("devMtIn");
 
-app.controller("homeCtrl", function($scope) {
-	$scope.myProfile = {
-		// name: "Alex DeCapri",
-		friends: [{name: 'Ryan'}, {name: 'Bryan'}, {name: 'Sarah'}, {name: 'Zac'}, {name: 'Erin'}]
-	};
+app.controller("homeCtrl", function($scope, profileService) {
+
+	//console.log(profileService);
+
+	$scope.myProfile = profileService.checkForProfile();
 
 	$scope.sortOptions = [{
 		display: "Ascending",
@@ -15,4 +15,23 @@ app.controller("homeCtrl", function($scope) {
 		value: true
 		}
 	];
+
+	//profileService.serviceTest(); //will show in console if connected properly
+
+	$scope.editing = false;
+
+
+	//if referenced in html, it HAS to live on $scope
+	//even though saveProfile is tied to profileService.js
+	$scope.saveProfile = function(profile) {
+		profileService.saveProfile(profile);
+		$scope.editing = false;
+	};
+
+	$scope.deleteProfile = function(profile) {
+		profileService.deleteProfile(profile);
+		$scope.myProfile = profileService.checkForProfile();
+	}
+
 });
+
